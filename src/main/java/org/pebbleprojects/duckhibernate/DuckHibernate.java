7a7bcee975258c37a7146de5012623c889aa3db2
NoTarget <notargetofficial@gmail.com>
2023-08-03 08:40:35 +0200

    Update

--- src/main/java/org/pebbleprojects/duckhibernate/DuckHibernate.java
+++ src/main/java/org/pebbleprojects/duckhibernate/DuckHibernate.java
@@ -3,31 +3,16 @@ package org.pebbleprojects.duckhibernate;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.World;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.pebbleprojects.duckhibernate.listeners.PlayerJoin;
-import org.pebbleprojects.duckhibernate.listeners.PlayerQuit;
 
 import java.util.Iterator;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public final class DuckHibernate extends JavaPlugin {
 
-    private Timer task;
     private int i = -1;
-    public static DuckHibernate INSTANCE;
 
     @Override
     public void onEnable() {
-        INSTANCE = this;
-
-        new Thread(this::startTimer).start();
-
-        final PluginManager pm = getServer().getPluginManager();
-
-        pm.registerEvents(new PlayerJoin(), this);
-        pm.registerEvents(new PlayerQuit(), this);
 
         getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
             if (getServer().getOnlinePlayers().size() == 0) {
@@ -47,7 +32,7 @@ public final class DuckHibernate extends JavaPlugin {
 
                     final Iterator<World> w = getServer().getWorlds().iterator();
 
-                    while(w.hasNext()) {
+                    while (w.hasNext()) {
                         final Chunk[] c;
                         int l = (c = w.next().getLoadedChunks()).length;
 
@@ -70,21 +55,4 @@ public final class DuckHibernate extends JavaPlugin {
         Bukkit.getScheduler().cancelTasks(this);
         getServer().shutdown();
     }
-
-    public void startTimer() {
-        task = new Timer();
-
-        task.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                if (getServer().getOnlinePlayers().size() == 0) getServer().shutdown();
-
-            }
-        }, 120000);
-    }
-
-    public void stopTimer() {
-        task.cancel();
-    }
-
 }
