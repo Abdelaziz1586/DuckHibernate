0bc8a0e790d0394c917987266cdd222674ab25c3
NoTarget <notargetofficial@gmail.com>
2023-08-06 20:26:04 +0200

    Update

--- src/main/java/org/pebbleprojects/duckhibernate/DuckHibernate.java
+++ src/main/java/org/pebbleprojects/duckhibernate/DuckHibernate.java
@@ -5,54 +5,53 @@ import org.bukkit.Chunk;
 import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Iterator;
-
 public final class DuckHibernate extends JavaPlugin {
 
-    private int i = -1;
+    private boolean b = false;
 
-    @Override
+    public void onDisable() {
+        try {
+            Bukkit.getScheduler().cancelTasks(this);
+        } catch (Throwable ignored) {
+        }
+    }
+
     public void onEnable() {
-
         getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
             if (getServer().getOnlinePlayers().size() == 0) {
-                if (i == -1) {
-                    final Iterator<World> w = getServer().getWorlds().iterator();
-
-                    while (w.hasNext()) w.next().save();
-
-                    i = 0;
+                if (!b) {
+                    for (World world : getServer().getWorlds()) world.save();
+                    b = true;
                     return;
                 }
-
                 try {
                     Thread.sleep(1000L);
-
-                    int u = 0;
-
-                    final Iterator<World> w = getServer().getWorlds().iterator();
-
-                    while (w.hasNext()) {
-                        final Chunk[] c;
-                        int l = (c = w.next().getLoadedChunks()).length;
-
-                        for(int i = 0; i < l; ++i) {
-                            if (c[i].unload(true)) u++;
-                        }
-                    }
-
-                    if (u > 0) System.gc();
-
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                    a();
+                } catch (Exception ignored) {
                 }
             }
         }, 0L, 1L);
+        a();
     }
 
-    @Override
-    public void onDisable() {
-        Bukkit.getScheduler().cancelTasks(this);
-        getServer().shutdown();
+    private void a() {
+        byte b = 0, b1;
+        int i;
+        for (World world : Bukkit.getWorlds()) {
+            Chunk[] arrayOfChunk;
+            for (i = (arrayOfChunk = (world.getLoadedChunks())).length, b1 = 0; b1 < i; b1++) {
+                if (arrayOfChunk[b1].unload(true))
+                    b++;
+            }
+        }
+        if (b > 0) {
+            b();
+        }
+    }
+
+    private void b() {
+        Runtime.getRuntime().freeMemory();
+        System.gc();
+        Runtime.getRuntime().freeMemory();
     }
 }
